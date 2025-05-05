146e81fc6a368349fe625e9d03425e17ea9d780f
Mohamed Maher <mohamedmaher@mohameds-air.speedport.ip>
2025-05-05 22:37:31 +0200

    data loading

--- train_cephalometric.py
+++ train_cephalometric.py
@@ -2,9 +2,8 @@
 import os
 import argparse
 import torch
-# import mmcv
-from mmcv import Config
-from mmcv import DictAction
+import mmcv
+from mmcv import Config, DictAction
 from mmcv.runner import init_dist, set_random_seed
 from mmpose.apis import train_model
 from mmpose.datasets import build_dataset
